3a73693827956460c631fba5c58fd581d4fa8146
WennerLeonn <wennersantana@seplag.mt.gov.br>
2023-04-14 12:29:02 -0400

    outro teste

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,7 @@ import model.entities.Department;
 public class Program {
 
 	public static void main(String[] args) {
-		// testes
+		
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
 
